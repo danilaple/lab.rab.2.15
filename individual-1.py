d19df9eade88bd1700567efcac0dd3303f42db3f
danilaple <99321805+danilaple@users.noreply.github.com>
2022-12-26 16:35:51 +0300

    Update individual-1.py

--- individual-1.py
+++ individual-1.py
@@ -6,11 +6,11 @@
 
 if __name__ == "__main__":
 
-    with open('input.txt', 'r') as file:
-        f = file.read().replace('0', 'ноль').replace('1', 
+    file = open('input.txt', 'r')
+    print(file.read().replace('0', 'ноль').replace('1', 
         'один').replace('2', 'два').replace('3', 'три').replace('4', 
         'четыре').replace('5', 'пять').replace('6', 'шесть').replace('7',
-        'семь').replace('8', 'восемь').replace('9', 'девять')
-    print(f)
+        'семь').replace('8', 'восемь').replace('9', 'девять'))
+    file.close()
 
 
